1cbefb3a4f92c27020e1b03eb8ae5ffd5134fdf5
Richard Trinh <richardtrinh195@csu.fullerton.edu>
2026-02-27 21:26:28 -0800

    AI-43 Added code related to remote event handling that handles messages sent and received.

--- src/server/ChatbotServer.server.lua
+++ src/server/ChatbotServer.server.lua
@@ -6,3 +6,18 @@ local Query = require(ReplicatedStorage.Shared.Query)
 local Remotes = ReplicatedStorage:WaitForChild("Remotes")
 local ChatbotRequest = Remotes:WaitForChild("ChatbotRequest")
 local ChatbotResponse = Remotes:WaitForChild("ChatbotResponse")
+
+-- Fired when a player sends a message from the UI.
+ChatbotRequest.OnServerEvent:Connect(function(player, message)
+
+    -- Ask the AI model for a reply using message.
+    local reply = Query.AskAI(message)
+
+    -- Fallback if the AI fails
+    if reply == -1 then
+        reply = "Sorry, AI model is currently unreachable."
+    end
+
+    -- Send the AI's message response (or error message) back to the player.
+    ChatbotResponse:FireClient(player, reply)
+end)
