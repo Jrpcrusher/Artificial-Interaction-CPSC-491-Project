20ffa7b38a1b1ae163b6c48502cc0b224c936030
Richard Trinh <richardtrinh195@csu.fullerton.edu>
2026-02-27 16:57:48 -0800

    AI-42 Added function to display the message on the scrolling frame responsible for message history.

--- src/client/ui/AIChatGui/Scripts/ChatController.client.lua
+++ src/client/ui/AIChatGui/Scripts/ChatController.client.lua
@@ -1,4 +1,4 @@
--- References to UI Elements
+--[[References to UI Elements]]
 local scriptsFolder = script.Parent
 local gui = scriptsFolder.Parent    -- References AIChatGui (ScreenGui)
 
@@ -14,8 +14,22 @@ local messageHistory = chatFrame:WaitForChild("MessageHistory")
 local playerMessageTemplate = chatFrame:WaitForChild("PlayerMessageTemplate")
 local aiMessageTemplate = chatFrame:WaitForChild("AIMessageTemplate")
 
--- Services & Remotes
+--[[Services & Remotes]]
 local UserInputService = game:GetService("UserInputService")
---[[ Below code will be uncommented once remote events are created in my local project. This will also need to be done in the main project. ]]
+--[Below code will be uncommented once remote events are created in my local project. This will also need to be done in the main project.]
 -- local ChatbotRequest = game.ReplicatedStorage:WaitForChild("ChatbotRequest")
 -- local ChatbotResponse = game.ReplicatedStorage:WaitForChild("ChatbotResponse")
+
+--[[Message Display]]
+local function addMessage(text, isPlayer)
+    -- Chooses the template to use based on message being sent/received.
+    local message = (isPlayer and playerMessageTemplate or aiMessageTemplate):Clone()
+
+    message.Visible = true
+    message.Text = text
+    message.Parent = messageHistory
+
+    -- Automatically moves to the bottom of the message history.
+    task.wait()
+    messageHistory.CanvasPosition = Vector2.new(0, messageHistory.AbsoluteCanvasSize.y)
+end
