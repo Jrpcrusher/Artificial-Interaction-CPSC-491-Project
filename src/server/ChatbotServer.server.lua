059299bdebfaa0b46503f3ffc7475f38b2a5aced
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-03-07 01:45:29 -0800

    AI-88 Added logic for saving AI messages

--- src/server/ChatbotServer.server.lua
+++ src/server/ChatbotServer.server.lua
@@ -2,7 +2,10 @@ print("ChatbotServer loaded.")
 
 -- Reference to module script connecting AI chat bot to the game.
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
 local Query = require(ReplicatedStorage.Shared.Query)
+local MessageManager = require(ReplicatedStorage.Shared.MessageManager)
+local TranscriptManager = require(ServerScriptService.Server.TranscriptManager)
 
 -- References to remote events in ReplicatedStorage.
 local Remotes = ReplicatedStorage:WaitForChild("Remotes")
@@ -11,15 +14,19 @@ local ChatbotResponse = Remotes:WaitForChild("ChatbotResponse")
 
 -- Fired when a player sends a message from the UI.
 ChatbotRequest.OnServerEvent:Connect(function(player, message)
+	-- Ask the AI model for a reply using message.
+	local reply = Query.AskAI(message)
+	local time_value = os.time()
+	local _, debug_statement = TranscriptManager.Add(MessageManager.Create("AI", time_value, reply))
+	TranscriptManager.Save()
+	print(TranscriptManager.Get())
+	print(debug_statement)
 
-    -- Ask the AI model for a reply using message.
-    local reply = Query.AskAI(message)
+	-- Fallback if the AI fails
+	if reply == -1 then
+		reply = "Sorry, AI model is currently unreachable."
+	end
 
-    -- Fallback if the AI fails
-    if reply == -1 then
-        reply = "Sorry, AI model is currently unreachable."
-    end
-
-    -- Send the AI's message response (or error message) back to the player.
-    ChatbotResponse:FireClient(player, reply)
+	-- Send the AI's message response (or error message) back to the player.
+	ChatbotResponse:FireClient(player, reply)
 end)
