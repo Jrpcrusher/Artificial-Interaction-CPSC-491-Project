78312b333d756ef3ef0da553af12f080d74834f3
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-04-16 22:23:15 -0700

    AI-193 Implemented check for talking to AI task completion

--- src/server/ChatbotServer.server.lua
+++ src/server/ChatbotServer.server.lua
@@ -4,6 +4,7 @@ local SoundService = game:GetService("SoundService")
 local Query = require(ReplicatedStorage.Shared.Utils.Chat.Query)
 local MessageManager = require(ReplicatedStorage.Shared.Utils.Chat.MessageManager)
 local TranscriptManager = require(ReplicatedStorage.Shared.Utils.Transcript.TranscriptManager)
+local TaskService = require(ReplicatedStorage.Shared.Systems.Tasks.TaskService)
 
 local Remotes = ReplicatedStorage:WaitForChild("Remotes")
 local ChatRemotes = Remotes:WaitForChild("Chat")
@@ -28,14 +29,21 @@ ChatbotRequest.OnServerEvent:Connect(function(player, message)
 	end
 
 	local timeValue = os.time()
+	print("Before save")
 	TranscriptManager.Add(player.UserId, MessageManager.Create(player.UserId, timeValue, message))
+	print("Before after save")
 	SendMessage:Play()
 	local reply = Query.AskAI(message)
+
+	print("after send")
 	Reply:Play()
 
 	if reply == -1 or reply == nil then
 		reply = "Sorry, AI model is currently unreachable."
 	end
+
+	local sceneNumber = player:GetAttribute("Scene")
+	TaskService.CompleteTask(player, sceneNumber, "talk_to_ai")
 
 	timeValue = os.time()
 	TranscriptManager.Add(player.UserId, MessageManager.Create(0, timeValue, reply))
