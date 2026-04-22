81e1d741eac0ed2d949cd28ff76585a8cb8ac152
Jrpcr <JasonPalomino@csu.fullerton.edu>
2026-04-21 23:36:13 -0700

    AI-195 Fixed dialogue not waiting to be finished before next event

--- src/shared/Systems/Dialogue/DialogueHandler.lua
+++ src/shared/Systems/Dialogue/DialogueHandler.lua
@@ -97,7 +97,7 @@ function DialogueHandler:_showNode(nodeName) -- Function that shows the nodes as
 	self:_typewrite(node)
 end
 
-function DialogueHandler:Start(dialogueTree, startNode) -- Function to kick off the dialogue gui
+function DialogueHandler:Start(dialogueTree, dialogueName, startNode)
 	if self.isActive then
 		return
 	end
@@ -118,19 +118,23 @@ function DialogueHandler:Start(dialogueTree, startNode) -- Function to kick off 
 		warn("Start node not found:", initialNode)
 		return
 	end
-	self.startNode = startNode or "start"
+
+	self.dialogueName = dialogueName
+	self.startNode = initialNode
 	self.currentTree = dialogueTree
+
 	GuiMouseManager.OpenGui()
 	GuiMovementManager.Lock()
 	self:_showNode(initialNode)
 end
 
-function DialogueHandler:Stop() -- Stop the GUI
+function DialogueHandler:Stop()
 	if not self.isActive then
 		return
 	end
 
 	self.isActive = false
+	self.dialogueName = nil
 	self.startNode = nil
 	self.currentTree = nil
 	self.currentNode = nil
@@ -201,10 +205,11 @@ function DialogueHandler:_connectDialogueClick()
 		if self.canAdvanceLinear and node.next then
 			self:_showNode(node.next)
 		elseif self.isFinalNode then
+			local finishedDialogue = self.dialogueName
 			local finishedStartNode = self.startNode
-			print("CLIENT firing FinishDialogue", finishedStartNode)
+			print("CLIENT firing FinishDialogue", finishedDialogue, finishedStartNode)
 			self:Stop()
-			FinishDialogue:FireServer(finishedStartNode)
+			FinishDialogue:FireServer(finishedDialogue, finishedStartNode)
 		end
 	end)
 end
